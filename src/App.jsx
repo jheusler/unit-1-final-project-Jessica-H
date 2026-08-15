e2e60f7900a0c6a16d62ce3582df96c9ab39a1f0
Jessica Heusler <Jessica.Heusler@gmail.com>
2026-08-14 22:38:04 -0500

    Add gallery and about page routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
+import { Link, Route, Routes } from "react-router";
 import "./App.css";
+import About from "./components/About.jsx";
 import Gallery from "./components/Gallery.jsx";
 import paintings from "./data/paintings.js";
 
 function App() {
-  return <Gallery paintings={paintings} />;
+  return (
+    <>
+      <nav>
+        <Link to="/">Gallery</Link>
+        {" | "}
+        <Link to="/about">About</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Gallery paintings={paintings} />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/About.jsx
@@ -0,0 +1,10 @@
+function About() {
+  return (
+    <main>
+      <h1>About the Artist</h1>
+      <p>This gallery showcases original watercolor paintings.</p>
+    </main>
+  );
+}
+
+export default About;
